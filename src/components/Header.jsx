038cfbf6bffc07f6489651d414a347e90d678a8e
Antor Web-Dev <antorremeni@gmail.com>
2025-09-13 23:17:51 +0600

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { LuMapPin } from "react-icons/lu";
 import { TfiEmail } from "react-icons/tfi";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -14,16 +16,24 @@ const Header = () => {
 
                 {/* ------------ left-side */}
 
-                <div>
-                    <div>
-                        <LuMapPin />
-                        <p>254 Lillian Blvd, Holbrook</p>
+                <div className='flex'>
+                    <div className='flex gap-[9px] items-center border-r border-[#ffffff68] pr-[9px]'>
+                        <LuMapPin className='text-[16px] ' />
+                        <p className='text-[14px] font-normal font-primary text-white'>254 Lillian Blvd, Holbrook</p>
                     </div>
+                    <div className='flex gap-[8px] items-center pl-[11px]'>
+                        <TfiEmail className='text-[16px]' />
+                        <p className='text-[14px] font-normal font-primary text-white'>info@santizex-site.com</p>
+                    </div>
+                </div>
 
-                    <div>
-                        <TfiEmail />
-                        <p>info@santizex-site.com</p>
-                    </div>
+                {/* ---------- right-side */}
+
+                <div>
+                    <FaFacebookF />
+                    <FaTwitter />
+
+                    <button className='w-[137px] bg-[#FF5A3C]'>Add Listings</button>
 
                 </div>
 
